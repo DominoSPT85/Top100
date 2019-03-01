b57839062fbcc30f8fc69287d01632290e290489
Shandra <shandradev85@gmail.com>
2019-02-28 20:48:24 -0700

    updated artist controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,31 +1,44 @@
 class ArtistsController < ApplicationController
-  
+  before_action :find_artist, only: [:show, :update, :edit, :destroy]
 
   def index
     @artists = Artist.all
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def new
     @artist = Artist.new
+
+    #render partial: 'artists/form'
   end
 
   def create
-    
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      redirect_to artists_path
+    else
+      render :new
+    end
   end
 
   def edit
-
+    #render partial: 'artists/form'
   end
 
   def update
-
+    if @artist.update(artist_params)
+      redirect_to artists_path
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @artist.destroy
+    redirect_to artist_path
 
   end
 
@@ -33,6 +46,13 @@ class ArtistsController < ApplicationController
 
   private
 
+      def artist_params
+        params.require(:artist).permit(:name)
+      end
+
+      def find_artist
+        @artist = Artist.find(params[:id])
+      end
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :songs
   end
 
-  resources :billboards do
+  resources :songs do
     resources :songs
   end
   
