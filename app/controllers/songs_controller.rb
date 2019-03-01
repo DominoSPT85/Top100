9d90d39e3a777a5a65de3b0f480ff2eefbe25e1e
Shandra <shandradev85@gmail.com>
2019-02-28 22:16:09 -0700

    updated songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,10 @@
 class SongsController < ApplicationController
+    before_action :set_artist
 
+    before_action :set_song, only [:show, :update, :edit, :destroy]
 
   def index
-
+    @songs = @artist.songs
   end
 
   def show
@@ -10,29 +12,52 @@ class SongsController < ApplicationController
   end
 
   def new
-
+    @song = @artist.songs.new
+    #render partial: 'songs/form'
   end
 
   def create 
+    @song = @artist.songs.new(songs_params)
+
+    if @song.save
+      redirect_to [@artist, @song]
+    else
+      render :new
+    end
 
   end
 
   def edit
-
+    #render partial: 'songs/form'
   end
 
   def update
+    if @song.update(song_params)
+      redirect_to [@artist, @song]
+    else
+      render :edit
+    end
 
   end
 
-  def delete
-
+  def destroy
+    @song.destroy
+    redirect_to artist_songs_path
   end
 
 
 
   private
 
+    def song_params
+      params.require(:song).permit(:title, :rank)
+    end
 
+    def set_artist
+      @artist = Artist.find(params[:artist_id])
+    end
 
+    def set_song
+      @song = Song.find(params[:id])
+    end
 end
