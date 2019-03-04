72ec61c1ae59f640db9c1ae7809af1aa5d7346c2
Shandra <shandradev85@gmail.com>
2019-03-03 18:07:37 -0700

    ran migration to add billboard id to artist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_031813) do
+ActiveRecord::Schema.define(version: 2019_03_04_010205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_03_01_031813) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "billboard_id"
+    t.index ["billboard_id"], name: "index_artists_on_billboard_id"
   end
 
   create_table "billboards", force: :cascade do |t|
@@ -38,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_03_01_031813) do
     t.index ["billboard_id"], name: "index_songs_on_billboard_id"
   end
 
+  add_foreign_key "artists", "billboards"
   add_foreign_key "songs", "artists"
   add_foreign_key "songs", "billboards"
 end
